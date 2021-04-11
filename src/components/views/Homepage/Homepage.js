fccd3883129278cc8305d499054b6d4653dc17e2
Damian <damianfilipek81@gmail.com>
2021-04-11 21:55:37 +0200

    Add homepage and login styles/demo contents

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,11 +1,64 @@
 import React from 'react';
 import styles from './Homepage.module.scss';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const demoContent = [
+  { tableId: '1', status: null, id: null, hour: '12:00' },
+  { tableId: '2', status: 'booked', id: '333', hour: '12:00' },
+  { tableId: '3', status: 'event', id: '444', hour: '12:00' },
+  { tableId: '1', status: 'event', id: '222', hour: '12:30' },
+  { tableId: '2', status: 'booked', id: '111', hour: '12:30' },
+  { tableId: '3', status: 'booked', id: '555', hour: '12:30' },
+  { tableId: '1', status: null, id: null, hour: '13:00' },
+  { tableId: '2', status: 'booked', id: '431', hour: '13:00' },
+  { tableId: '3', status: null, id: null, hour: '13:00' },
+  { tableId: '1', status: null, id: null, hour: '13:30' },
+  { tableId: '2', status: null, id: null, hour: '13:30' },
+  { tableId: '3', status: 'event', id: '132', hour: '13:30' },
+  { tableId: '1', status: 'booked', id: '124', hour: '14:00' },
+  { tableId: '2', status: 'booked', id: '321', hour: '14:00' },
+  { tableId: '3', status: 'event', id: '123', hour: '14:00' },
+];
+
+const demoOrders = {
+  locally: '25',
+  delivery: '15',
+};
 
 const Homepage = () => {
   return (
-    <div className={styles.component}>
-      <h2>Homepage view</h2>
-    </div>
+    <Paper className={styles.component}>
+      <h2>Todays orders statistics</h2>
+      <div className={styles.wrapper}>
+        <p>Locally orders: {demoOrders.locally}</p>
+        <p>Delivery orders: {demoOrders.delivery}</p>
+      </div>
+      <h2>Todays bookings and events</h2>
+      <div className={styles.wrapper}>
+        <List className={styles.table}>
+          <h4>Booked tables</h4>
+          {demoContent.map(data => (
+            data.status === 'booked' ? (
+              <ListItem key={data.id}>
+                <ListItemText primary={`${data.hour} Table ${data.tableId}`} />
+              </ListItem>) : null
+          ))}
+        </List>
+        <List className={styles.table}>
+          <h4>Event-booked tables</h4>
+          {demoContent.map(data => (
+            data.status === 'event' ? (
+              <ListItem key={data.id}>
+                <ListItemText primary={`${data.hour} Table ${data.tableId}`} />
+              </ListItem>) : null
+          ))}
+        </List>
+      </div>
+    </Paper>
   );
 };
 
--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className={styles.component}>
-      <h2>Login view</h2>
+      <form className={styles.login}>
+        <h4>Login</h4>
+        <TextField id="standard-basic" label="Login" />
+        <TextField id="standard-basic" label="Password" />
+        <Button variant="outlined" component={Link} to={`/`}>Login</Button>
+      </form>
     </div>
   );
 };
